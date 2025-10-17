10b8ab46e940140863d0e3c81e70b69ed35653da
Meliodas1827 <dasmelio564@gmail.com>
2025-10-17 12:14:57 +0800

    phase 1 inquiry

--- src/app/(crew)/crew/inquiry/page.tsx
+++ src/app/(crew)/crew/inquiry/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navigation from "@/components/Navigation";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="min-h-screen">
+      <Navigation currentPath="/profile" />
+    </div>
+  );
 }
 
 export default page;
